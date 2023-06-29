2aab82b0d86791c08031654accd946a23fd24fb7
심재훈 <model8228@naver.com>
2023-06-29 09:31:50 +0900

    test

--- src/p03/Lotto3.java
+++ src/p03/Lotto3.java
@@ -6,11 +6,11 @@ import java.util.Scanner;
 public class Lotto3 {
 
 	public static void main(String[] args) {
-		int count=0;
+		
 		Random random=new Random();
 		int[] lotto=new int[6];
 		for(int i=0;i<lotto.length;i++) {
-			lotto[i]=random.nextInt(6)+1;
+			lotto[i]=random.nextInt(45)+1;
 			System.out.println(lotto[i]);
 		}
 		
@@ -18,14 +18,17 @@ public class Lotto3 {
 		System.out.println("1~45까지의 6개의 숫자를 입력하세요");
 		String lottoStr=scan.nextLine();	
 		String[] strs=lottoStr.split(",");
+		int count=0;
 		for(int i=0;i<strs.length;i++) {
+			
 			int num=Integer.parseInt(strs[i]);
-			System.out.println(strs[i]);
+			for(int j=0;j<lotto.length;j++) {
 			
 			if (lotto[i]==num ) {
 				count++;
 				break;
 			} 
+			}
 			
 		}
 		System.out.println(count+"개 맞췄습니다.");
